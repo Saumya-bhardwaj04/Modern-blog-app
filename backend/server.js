4bf9bc72e8b3f8d8d64bb376a5b8a735d56de10f
Saumya-bhardwaj04 <samisharma000@gmail.com>
2026-01-15 16:18:13 +0530

    allow PATCH method in CORS

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const allowedOrigins = process.env.ALLOWED_ORIGINS
 app.use(cors({
     origin: allowedOrigins,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 app.get("/", (req, res) => {
