5d819cabc1460c6d59ce8c9d6798258902d7eb8e
Saumya-bhardwaj04 <samisharma000@gmail.com>
2026-01-17 19:05:02 +0530

    fix: setting

--- frontend/src/components/Setting.jsx
+++ frontend/src/components/Setting.jsx
@@ -67,69 +67,71 @@ function Setting() {
   return token == null ? (
     <Navigate to={"/"} />
   ) : (
-    <div className="pt-[72px] min-h-[calc(100vh-72px)] w-full p-5 md:w-[800px] mx-auto flex flex-col items-center md:justify-center ">
-      <div className="w-full">
-        <h1 className=" mt-4 mb-8 text-2xl font-semibold ">Settings</h1>
-      </div>
-      <div className="my-4 w-full">
-        <h2 className="text-2xl font-semibold my-2">Show Saved Blogs ?</h2>
-        <select
-          value={data.showSavedBlogs}
-          name=""
-          id=""
-          className="w-full p-3 rounded-lg border text-lg focus:outline-none"
-          onChange={(e) =>
-            setData((prev) => ({
-              ...prev,
-              showSavedBlogs: e.target.value == "true" ? true : false,
-            }))
-          }
-        >
-          <option value="true">True</option>
-          <option value="false">False</option>
-        </select>
-      </div>
-      <div className="my-4 w-full">
-        <h2 className="text-2xl font-semibold my-2">Show Liked Blogs ?</h2>
-        <select
-          value={data.showLikedBlogs}
-          name=""
-          id=""
-          className="w-full p-3 rounded-lg border text-lg focus:outline-none"
-          onChange={(e) =>
-            setData((prev) => ({
-              ...prev,
-              showLikedBlogs: e.target.value == "true" ? true : false,
-            }))
-          }
-        >
-          <option value="true">True</option>
-          <option value="false">False</option>
-        </select>
-      </div>
-      <button
-        className="bg-blue-500 text-lg py-4 px-7 rounded-full  font-semibold text-white my-6 "
-        onClick={handleVisibility}
-      >
-        Update
-      </button>
-      <hr className="my-6" />
-      <div className="border border-red-500 p-6 rounded-lg">
-        <h2 className="text-red-600 text-xl font-bold mb-3">
-          Danger Zone
-        </h2>
-        <p className="text-gray-700 mb-4">
-          Deleting your account will permanently remove:
-          <br />• Your profile
-          <br />• All blogs you created
-          <br />• Likes, followers & comments
-        </p>
+    <div className="pt-[72px] min-h-screen w-full">
+      <div className="min-h-[calc(100vh-72px)] flex justify-center">
+        <div className="w-full md:w-[800px] p-5 flex flex-col justify-center md:-translate-y-6">
+          <h1 className=" mb-8 text-2xl font-semibold ">Settings</h1>
+        </div>
+        <div className="my-4 w-full">
+          <h2 className="text-2xl font-semibold my-2">Show Saved Blogs ?</h2>
+          <select
+            value={data.showSavedBlogs}
+            name=""
+            id=""
+            className="w-full p-3 rounded-lg border text-lg focus:outline-none"
+            onChange={(e) =>
+              setData((prev) => ({
+                ...prev,
+                showSavedBlogs: e.target.value == "true" ? true : false,
+              }))
+            }
+          >
+            <option value="true">True</option>
+            <option value="false">False</option>
+          </select>
+        </div>
+        <div className="my-4 w-full">
+          <h2 className="text-2xl font-semibold my-2">Show Liked Blogs ?</h2>
+          <select
+            value={data.showLikedBlogs}
+            name=""
+            id=""
+            className="w-full p-3 rounded-lg border text-lg focus:outline-none"
+            onChange={(e) =>
+              setData((prev) => ({
+                ...prev,
+                showLikedBlogs: e.target.value == "true" ? true : false,
+              }))
+            }
+          >
+            <option value="true">True</option>
+            <option value="false">False</option>
+          </select>
+        </div>
         <button
-          onClick={handleDeleteAccount}
-          className="bg-red-600 hover:bg-red-700 text-white px-6 py-3 rounded-md"
+          className="bg-blue-500 text-lg py-4 px-7 rounded-full  font-semibold text-white my-6 "
+          onClick={handleVisibility}
         >
-          Delete Account
+          Update
         </button>
+        <hr className="my-6" />
+        <div className="border border-red-500 p-6 rounded-lg">
+          <h2 className="text-red-600 text-xl font-bold mb-3">
+            Danger Zone
+          </h2>
+          <p className="text-gray-700 mb-4">
+            Deleting your account will permanently remove:
+            <br />• Your profile
+            <br />• All blogs you created
+            <br />• Likes, followers & comments
+          </p>
+          <button
+            onClick={handleDeleteAccount}
+            className="bg-red-600 hover:bg-red-700 text-white px-6 py-3 rounded-md"
+          >
+            Delete Account
+          </button>
+        </div>
       </div>
     </div>
   );
