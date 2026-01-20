e10668d6a27c6343d5b312280513473d1b92f1e0
Saumya-bhardwaj04 <samisharma000@gmail.com>
2026-01-21 03:22:31 +0530

    fix:route error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import SearchBlogs from "./components/SearchBlogs";
 import Setting from "./components/Setting";
 import StartPage from "./pages/StartPage";
 import { useEffect } from "react";
+import Notifications from "./components/Notifications";
 
 function App() {
   useEffect(() => {
